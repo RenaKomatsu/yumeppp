97843a7c7afde9818681e2c61fbd56ef2e20fd23
renakomatsu <koma8r@gmail.com>
2020-08-22 22:36:57 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+<article class="pa3 pa5-ns" data-name="slab-stat-large">
+
+  <div class="cf">
+    <dl class="db dib-l w-auto-l lh-title mr6-l">
+ 
+      <dd class="f2 f-subheadline-l fw6 ml0">RENA dayo</dd>
+    </dl>
+    <dl class="db dib-l w-auto-l lh-title mr6-l">
+      <dd class="f2 f-subheadline-l fw6 ml0">YUMEP kawaii</dd>
+    </dl>
+    <dl class="db dib-l w-auto-l lh-title mr6-l">
+      <dd class="f2 f-subheadline-l fw6 ml0">saikou</dd>
+    </dl>
+    <dl class="db dib-l w-auto-l lh-title mr6-l">
+      <dd class="f6 fw4 ml0">Days Left</dd>
+      <dd class="f2 f-subheadline-l fw6 ml0">4</dd>
+    </dl>
+    <dl class="db dib-l w-auto-l lh-title mr6-l">
+      <dd class="f6 fw4 ml0">Favorite Cat</dd>
+      <dd class="f2 f-subheadline-l fw6 ml0">All of Them</dd>
+    </dl>
+    <dl class="db dib-l w-auto-l lh-title">
+      <dd class="f6 fw4 ml0">App Downloads</dd>
+      <dd class="f2 f-subheadline-l fw6 ml0">1,200</dd>
+    </dl>
+  </div>
+</article>
+
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <div>
+      <main class="cf pa2">
+        <img width="800" height="450" src="https://lh3.googleusercontent.com/VV1Q3rLQLKZkq7S23x3NV5xM_GuWQzS_EEIasKLpBVpBktl5_FT5CT8bfudd87JT8b8CpBT_dHX2Rd8vUuKGZQrBYajhoD25gweRmoORwGgxFowWqeKXMg0TNV75ymAiINoasXBbjm0=w800-h450" >
+    <div class="fl w-100 w-50-ns ph2">
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters030211_960.jpg"></a>
+      <a href="" class="no-underline pv2  db"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/elevaters.jpg"></a>
+      <a href="#" class="no-underline pv2  db"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/pink-and-noseworthy-22.12.10-cargo_960.jpg"/></a>
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg"></a>
+    </div>
+    <div class="fl w-50 w-25-ns ph2">
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/adam-stern-031209_960-2.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hw18-240112-cc_960.jpg"> </a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hw-residency-cargo_960.jpg"></a>
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/orchid-2-mnkr_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/O270711_960-2.jpg"></a>
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/adam-stern-020510_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/adam-stern-130610_960.jpg"></a>
+    </div>
+    <div class="fl w-50 w-25-ns ph2">
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/zach-hurd-101218_960.jpg"></a>
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/zh170311.4.cargo_960.jpg"></a>
+      <a href="" class="pv2 db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hwspringtour-cargo_960-1.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/cc-shanee_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/ZachHurd-190111s_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/hw170211pie-cargo_960.jpg"></a>
+      <a href="" class="pv2  db no-underline black"><img class="db w-100" src="https://s3-us-west-2.amazonaws.com/prnt/adam-stern-191110_960.jpg"></a>
+    </div>
+</main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
